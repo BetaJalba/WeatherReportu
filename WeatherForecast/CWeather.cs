278e3badf34451848caefe71f1796d5c933cd156
rossosbiadito <168523942+rossosbiadito@users.noreply.github.com>
2025-01-10 10:39:28 +0100

    Fix API error

--- WeatherForecast/CWeather.cs
+++ WeatherForecast/CWeather.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -14,7 +15,8 @@ namespace WeatherForecast
         {
             using (WebClient client = new WebClient())
             {
-                return System.Text.Json.JsonSerializer.Serialize(JsonDocument.Parse(client.DownloadString($"https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}&start_date=2010-01-01&end_date=2024-12-01&hourly=temperature_2m")), new JsonSerializerOptions() { WriteIndented = true }); ;
+                string request = $"https://archive-api.open-meteo.com/v1/archive?latitude={lat.ToString(CultureInfo.CreateSpecificCulture("en-GB"))}&longitude={lon.ToString(CultureInfo.CreateSpecificCulture("en-GB"))}&start_date=2010-01-01&end_date=2024-12-01&hourly=temperature_2m";
+                return System.Text.Json.JsonSerializer.Serialize(JsonDocument.Parse(client.DownloadString(request)), new JsonSerializerOptions() { WriteIndented = true }); ;
             }
         }
     }
@@ -25,7 +27,8 @@ namespace WeatherForecast
         {
             using (WebClient client = new WebClient())
             {
-                return System.Text.Json.JsonSerializer.Serialize(JsonDocument.Parse(client.DownloadString($"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&hourly=pm10&start_date=2013-01-01&end_date=2024-12-01")), new JsonSerializerOptions() { WriteIndented = true }); ;
+                string request = $"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat.ToString(CultureInfo.CreateSpecificCulture("en-GB"))}&longitude={lon.ToString(CultureInfo.CreateSpecificCulture("en-GB"))}&hourly=pm10&start_date=2013-01-01&end_date=2024-12-01";
+                return System.Text.Json.JsonSerializer.Serialize(JsonDocument.Parse(client.DownloadString(request)), new JsonSerializerOptions() { WriteIndented = true }); ;
             }
         }
     }
